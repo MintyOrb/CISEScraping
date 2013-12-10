4b36aa6d7d0f008c3b30052fa6a01fd69a2ecc5e
Tim Borny <bornytm@gmail.com>
2013-12-10 15:54:12 -0500

    email and 20 oldest items pipeline classes (neither completed)

--- CISEScraping/pipelines.py
+++ CISEScraping/pipelines.py
@@ -35,47 +35,67 @@ class saveToFile(object):
 
         return item
 
-class twentyOldest(object):
+class emailResults(object):
 
     def __init__(self):
-
-        #dispatcher.connect(self.spider_closed, signal=signals.spider_closed)
-
-        # open file
-        self.oldOutput = open('twenty_oldest_pages.txt', 'wb')
-
-        # write table header
-        line = "{0:15} {1:40} {2:} \n\n".format("Domain","Last Updated","URL")
-
-        # create dict for storing oldest pages
-        self.oldest = {'lastUpdatedDateTime':0}
-
-    def process_item(self, item, spider):
-        
-        if item['group'] == "Old Page":
-            # get list of dates from list of oldest pages 
-            listOfValues = [x['lastUpdatedDateTime'] for x in self.oldest]
-            # id item is older than youngest item in the dict, remove that item and add the new one
-            if item['lastUpdatedDateTime'] < min(listOfValues):
-                if len(self.oldest) > 20:
-                    # delete current 'youngest'
-                    place = self.oldest.index(min(self.oldest))
-                    del self.oldest[place]
-                # add new item
-                self.oldest.append(item)
-
-        return item
+        # I need to figure out how to inatialize 
+        mailer = MailSender()
 
     def spider_closed(JMUCISE):
+        # write email body
 
-        # sort the array based on age
-        self.oldest = sorted(self.oldest, key=lambda k: k['lastUpdatedDateTime']) 
+        # send email
+        mailer.send(to=["radochlm@jmu.edu"], subject="CISE Site Crawling Spider Delivery", body="Some body", cc=["bornytm@gmail.com"])
+    
+    # for testing purposes
+    # def spider_opened(JMUCISE):
+    #         mailer.send(to=["bornytm@gmail.com"], subject="test email", body="Some body")
 
-        # write the dict to the file created
-        for item in oldest:
-            line = "{0:15} {1:40} {2:} \n".format(item['domain'],item["lastUpdated"],item["url"])
-            self.oldOutput.write(line)
 
-        return item
 
+# the following is the class that is meant to write a seperate file for the twenty or so oldest pages found during the crawl.
+# I ran into some issues and had to study for an exam rather than solve them.
+
+# class twentyOldest(object):
+
+#     def __init__(self):
+
+#         #dispatcher.connect(self.spider_closed, signal=signals.spider_closed)
+
+#         # open file
+#         self.oldOutput = open('twenty_oldest_pages.txt', 'wb')
+
+#         # write table header
+#         line = "{0:15} {1:40} {2:} \n\n".format("Domain","Last Updated","URL")
+
+#         # create dict for storing oldest pages
+#         self.oldest = {'lastUpdatedDateTime':0}
+
+#     def process_item(self, item, spider):
         
+#         if item['group'] == "Old Page":
+#             # get list of dates from list of oldest pages 
+#             listOfValues = [x['lastUpdatedDateTime'] for x in self.oldest]
+#             # id item is older than youngest item in the dict, remove that item and add the new one
+#             if item['lastUpdatedDateTime'] < min(listOfValues):
+#                 if len(self.oldest) > 20:
+#                     # delete current 'youngest'
+#                     place = self.oldest.index(min(self.oldest))
+#                     del self.oldest[place]
+#                 # add new item
+#                 self.oldest.append(item)
+
+#         return item
+
+#     def spider_closed(JMUCISE):
+
+#         # sort the array based on age
+#         self.oldest = sorted(self.oldest, key=lambda k: k['lastUpdatedDateTime']) 
+
+#         # write the dict to the file created
+#         for item in oldest:
+#             line = "{0:15} {1:40} {2:} \n".format(item['domain'],item["lastUpdated"],item["url"])
+#             self.oldOutput.write(line)
+
+#         return item
+
--- CISEScraping/settings.py
+++ CISEScraping/settings.py
@@ -4,5 +4,6 @@ SPIDER_MODULES = ['CISEScraping.spiders']
 NEWSPIDER_MODULE = 'CISEScraping.spiders'
 ITEM_PIPELINES = {
 	'CISEScraping.pipelines.saveToFile':500,
-	'CISEScraping.pipelines.twentyOldest':600,
+	# 'CISEScraping.pipelines.twentyOldest':600,
+	'CISEScraping.pipelines.emailResults':700
 	}
